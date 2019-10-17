50a3c5da5165fa24c91e72e8e17c16f8f6075c71
张奥 <2515912396@qq.com>
2019-10-17 16:09:48 +0800

    提交

---
+++ ClipImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClipImage
+//
+//  Created by 张奥 on 2019/10/17.
+//  Copyright © 2019 张奥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClipImage/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  ClipImage
+//
+//  Created by 张奥 on 2019/10/17.
+//  Copyright © 2019 张奥. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Masonry.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+  
+    
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
